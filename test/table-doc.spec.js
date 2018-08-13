75badc0957b054a3cbd6389e25bc9f3344068409
Andrew Tran <andrew@qualified.io>
2018-08-13 11:40:20 -0700

    Removed console logs. Updated test reflectig correct context

--- test/table-doc.spec.js
+++ test/table-doc.spec.js
@@ -4,10 +4,10 @@ import { expect } from 'chai'
 import { fixture } from './test-utils'
 
 describe('%table-doc', () => {
-  it('should render json as formatted markdown', () => {
+  it('should render a table in HTML format', () => {
     const example = process(marked, fixture('table-doc')).html();
-    expect(example).to.include('<td>id</td>\n<td>INT</td>');
-    expect(example).to.include('<tr>\n<td>title</td>\n<td>VARCHAR(100)</td>\n</tr>');
-    expect(example).to.include('<tr>\n<td>email</td>\n<td>VARCHAR(100)</td>\n</tr>');
+    expect(example).to.include('<td>id</td><td>INT</td>');
+    expect(example).to.include('<td>title</td><td>VARCHAR(100)</td>');
+    expect(example).to.include('<tr><td>email</td><td>VARCHAR(100)</td></tr>');
   });
 });
