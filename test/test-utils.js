194756904dc4e86932e9177dc1ad725a27fef48f
kazk <kazk.dev@gmail.com>
2021-10-21 13:27:11 -0700

    Update Jest

--- test/test-utils.js
+++ test/test-utils.js
@@ -1,13 +1,5 @@
-import jsdom from 'jsdom'
 import fs from 'fs'
 import { cwd } from 'process'
-
-// need to make CodeMirror happy by using JS-DOM
-const { JSDOM } = jsdom;
-const dom = new JSDOM('<body></body>');
-global.window = dom.window;
-global.document = dom.window.document;
-global.navigator = dom.window.navigator;
 
 const markdown = {}
 
